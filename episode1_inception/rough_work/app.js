312cd72fda977599667a5ce3a392ac4358e668cc
gyani <g.arya.mallick@gmail.com>
2023-10-15 13:07:25 +0530

    small task on creating sibling react elements

---
+++ episode1_inception/rough_work/app.js
@@ -0,0 +1,16 @@
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child1"}, [
+        React.createElement("h1", {}, "hello from h1"), 
+        React.createElement("h2", {}, "hello from h2")
+    ]),
+    React.createElement("div", {id: "child2"}, [
+        React.createElement("h1", {}, "hello from h1"),
+        React.createElement("h2", {}, "hello from h2")
+    ])
+]);
+
+console.log(parent);
+// const heading = React.createElement("h1", {}, "Hello world from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+// root.render(heading);

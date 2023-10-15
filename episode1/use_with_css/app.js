aab2aaa7d4f4a408f383b0f46a4c55514c7a66e8
gyani <g.arya.mallick@gmail.com>
2023-10-15 12:02:38 +0530

    implemented calling 'js' inside 'html' and outside 'html' by external js and css

---
+++ episode1/use_with_css/app.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", {id: "heading", name: "nood"}, "Hello world from React and css");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
